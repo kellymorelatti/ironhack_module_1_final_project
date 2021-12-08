8be77e9de4e71a96907ac946f9ce95e69a288c86
Kelly Morelatti <kelly.morelatti@gmail.com>
2021-12-08 10:34:14 -0300

    JS function to generate board

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
+//Make sure all HTML file is loaded before reading the script;
 document.addEventListener('DOMContentLoaded', () => {
+  
+  const grid = document.querySelector('.grid');  
+  let tiles = [];
+  let width = 10;
+
+  //Generate Board
+  function generateBoard(){
+    for (let i = 0; i < width*width; i++){
+      const tile = document.createElement('div'); //create 100 divs in HTML;
+      tile.setAttribute('id', i); //attribute an id to each tile;
+      grid.appendChild(tile); //insert each tile into the grid;
+      tiles.push(tile); //push tile to the 'tiles' array;
+    }
+  }
+  generateBoard();
+
+
+
 })
 
