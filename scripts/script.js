0c79afb16f0ef30a85752313cdc0f8aca653d824
Kelly Morelatti <kelly.morelatti@gmail.com>
2021-12-08 00:13:35 -0300

    Improving layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-// Display
-
-// 1. Popular o tabuleiro com quadradinhos/minas
-// 2. Clique com botão esquerdo nos quadradinhos
-//     revelar os quadradinhos
-// 3. Clique com botão direito nos quadradinhos
-//     marcar o quadrado
-// 4. Checar se ganhou/perdeu
+document.addEventListener('DOMContentLoaded', () => {
+})
 
